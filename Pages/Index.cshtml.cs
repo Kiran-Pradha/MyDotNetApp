bbda5c62b5cce1cffa7416516812d806aa3340fc
Kiran-Pradha <kiranpradha30@gmail.com>
2025-06-14 10:38:45 +0530

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyDotNetApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet(){
+    ViewData["Message"] = Environment.GetEnvironmentVariable("MY_NAME");
+    }
+}
